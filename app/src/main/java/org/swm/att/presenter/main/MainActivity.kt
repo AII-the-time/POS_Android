231cb1fd0f19219002faa6e8694cec3b45106a4a
915dbfl <dbfl00915@naver.com>
2023-07-07 18:43:11 +0900

    feat: apply dataBinding to mainActivity

--- app/src/main/java/org/swm/att/presenter/main/MainActivity.kt
+++ app/src/main/java/org/swm/att/presenter/main/MainActivity.kt
@@ -2,11 +2,18 @@ package org.swm.att.presenter.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.databinding.DataBindingUtil
+import dagger.hilt.android.AndroidEntryPoint
 import org.swm.att.R
+import org.swm.att.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
         setContentView(R.layout.activity_main)
     }
 }
