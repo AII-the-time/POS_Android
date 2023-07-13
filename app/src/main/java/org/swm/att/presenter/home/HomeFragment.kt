8dad998834af7a9c988339105c7455271e92245e
915dbfl <dbfl00915@naver.com>
2023-07-13 13:08:21 +0900

    feat: add HomeFragment
    하나의 main activity를 두고 그안에 fragment들을 배치해 네비게이션하고자 함.

---
+++ app/src/main/java/org/swm/att/presenter/home/HomeFragment.kt
@@ -0,0 +1,22 @@
+package org.swm.att.presenter.home
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import org.swm.att.R
+import org.swm.att.`common-ui`.BaseFragment
+import org.swm.att.databinding.FragmentHomeBinding
+
+class HomeFragment : BaseFragment<FragmentHomeBinding>(R.layout.fragment_home) {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        return binding.root
+    }
+
+}
