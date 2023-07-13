cd33e368dfe5a7e4b27423aa5aadec88699755d9
915dbfl <dbfl00915@naver.com>
2023-07-13 11:11:53 +0900

    feat: apply baseActivity to MainActivity

--- app/src/main/java/org/swm/att/presenter/main/MainActivity.kt
+++ app/src/main/java/org/swm/att/presenter/main/MainActivity.kt
@@ -1,19 +1,13 @@
 package org.swm.att.presenter.main
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.activity.viewModels
-import androidx.databinding.DataBindingUtil
-import dagger.hilt.android.AndroidEntryPoint
 import org.swm.att.R
+import org.swm.att.`common-ui`.BaseActivity
 import org.swm.att.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityMainBinding
+class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
-        setContentView(R.layout.activity_main)
     }
 }
