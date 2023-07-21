cc1349bf0c661e8dc8b4830f71b8ce5fe17cfbc2
915dbfl <dbfl00915@naver.com>
2023-07-21 13:35:37 +0900

    feat: remove statusBar

--- app/src/main/java/org/swm/att/presenter/MainActivity.kt
+++ app/src/main/java/org/swm/att/presenter/MainActivity.kt
@@ -1,6 +1,9 @@
 package org.swm.att.presenter
 
+import android.os.Build
 import android.os.Bundle
+import android.view.View
+import android.view.WindowInsets
 import dagger.hilt.android.AndroidEntryPoint
 import org.swm.att.R
 import org.swm.att.common_ui.BaseActivity
@@ -11,4 +14,19 @@ class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
+
+    override fun onResume() {
+        super.onResume()
+        removeStatusBar()
+    }
+
+    private fun removeStatusBar() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.setDecorFitsSystemWindows(false)
+            window.insetsController?.hide(WindowInsets.Type.statusBars())
+        } else {
+            window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
+        }
+    }
+
 }
