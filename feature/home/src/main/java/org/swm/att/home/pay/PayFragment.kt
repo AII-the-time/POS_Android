d8b9570c17c281421886a6d7a0c291e30a702831
915dbfl <dbfl00915@naver.com>
2023-08-08 23:51:11 +0900

    feat: implement pay fragment #22
    hardcoding된 text, 겹치는 레이아웃 수정 필요

---
+++ feature/home/src/main/java/org/swm/att/home/pay/PayFragment.kt
@@ -0,0 +1,9 @@
+package org.swm.att.home.pay
+
+import org.swm.att.common_ui.base.BaseFragment
+import org.swm.att.home.R
+import org.swm.att.home.databinding.FragmentPayBinding
+
+class PayFragment : BaseFragment<FragmentPayBinding>(R.layout.fragment_pay) {
+
+}
