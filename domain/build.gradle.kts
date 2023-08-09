c70625e99a86d1d286557de68ef4f043d5d82143
915dbfl <dbfl00915@naver.com>
2023-08-09 13:56:03 +0900

    feat: add serialization dependency to domain module #22

--- domain/build.gradle.kts
+++ domain/build.gradle.kts
@@ -1,9 +1,14 @@
 plugins {
     id("java-library")
     id("org.jetbrains.kotlin.jvm")
+    id("org.jetbrains.kotlin.plugin.serialization")
 }
 
 java {
     sourceCompatibility = JavaVersion.VERSION_17
     targetCompatibility = JavaVersion.VERSION_17
+}
+
+dependencies {
+    implementation(libs.kotlin.serialization)
 }
