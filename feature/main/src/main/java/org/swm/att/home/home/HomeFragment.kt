9c88134531bacec9f83c50297a814148420f10d2
915dbfl <dbfl00915@naver.com>
2023-09-01 17:05:46 +0900

    refactor: use navArg in HomeFragment #46

--- feature/main/src/main/java/org/swm/att/home/home/HomeFragment.kt
+++ feature/main/src/main/java/org/swm/att/home/home/HomeFragment.kt
@@ -6,14 +6,13 @@ import android.view.View
 import android.widget.Toast
 import androidx.annotation.RequiresApi
 import androidx.fragment.app.activityViewModels
+import androidx.navigation.fragment.navArgs
 import androidx.recyclerview.widget.DefaultItemAnimator
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.google.android.material.tabs.TabLayoutMediator
 import dagger.hilt.android.AndroidEntryPoint
 import org.swm.att.common_ui.base.BaseFragment
 import org.swm.att.common_ui.util.NetworkState
-import org.swm.att.common_ui.util.extension.customGetSerializable
-import org.swm.att.domain.entity.request.OrderedMenusVO
 import org.swm.att.home.R
 import org.swm.att.home.adapter.CategoryViewPagerAdapter
 import org.swm.att.home.adapter.SelectedMenuAdapter
@@ -26,6 +25,7 @@ class HomeFragment : BaseFragment<FragmentHomeBinding>(R.layout.fragment_home) {
     private lateinit var categoryViewPagerAdapter: CategoryViewPagerAdapter
     private lateinit var selectedMenuAdapter: SelectedMenuAdapter
     private val homeViewModel: HomeViewModel by activityViewModels()
+    private val args by navArgs<HomeFragmentArgs>()
 
     @RequiresApi(Build.VERSION_CODES.TIRAMISU)
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -39,11 +39,8 @@ class HomeFragment : BaseFragment<FragmentHomeBinding>(R.layout.fragment_home) {
         setOrderBtnListener()
     }
 
-    @RequiresApi(Build.VERSION_CODES.TIRAMISU)
     private fun setSelectedMenuList() {
-        val args = this.arguments
-        val selectedMenus = args?.customGetSerializable<OrderedMenusVO>("selectedMenus")
-        selectedMenus?.let {
+        args?.selectedMenus?.let {
             homeViewModel.setSelectedMenusVO(it)
         }
     }
