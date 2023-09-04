e44faa360d5b41a0874777333e365a9a4cbd875a
915dbfl <dbfl00915@naver.com>
2023-09-04 11:21:26 +0900

    feat: implement customCalendar logic #49
    - 커스텀 달력 구현
    - date picker 구현

---
+++ common-ui/src/main/java/org/swm/att/common_ui/util/calendar/CustomCalendar.kt
@@ -0,0 +1,107 @@
+package org.swm.att.common_ui.util.calendar
+
+import android.content.Context
+import android.content.ContextWrapper
+import android.util.AttributeSet
+import android.view.LayoutInflater
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.lifecycle.LifecycleOwner
+import org.swm.att.common_ui.databinding.CustomCalendarBinding
+import java.text.SimpleDateFormat
+import java.util.Calendar
+import java.util.Date
+import java.util.Locale
+
+class CustomCalendar @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = 0
+): ConstraintLayout(context, attrs, defStyleAttr) {
+    private lateinit var lifecycleOwner: LifecycleOwner
+    private lateinit var customCalendarAdapter: CustomCalendarAdapter
+    private var month: Int = -1
+    private var year: Int = -1
+    private var calInstance = Calendar.getInstance()
+    private val baseMonthFormatter = SimpleDateFormat("yyyy년 MM월", Locale.KOREA)
+    private val binding: CustomCalendarBinding by lazy {
+        CustomCalendarBinding.inflate(LayoutInflater.from(context), this, true)
+    }
+    private lateinit var customCalendarViewModel: CustomCalendarViewModel
+    private var dates = ArrayList<Date>()
+
+    init {
+        initControl()
+        setLifecycleOwner()
+    }
+
+    fun initCustomCalendarViewModel(viewModel: CustomCalendarViewModel) {
+        customCalendarViewModel = viewModel
+        initDatesRecyclerView()
+    }
+
+    private fun initDatesRecyclerView() {
+        customCalendarAdapter = CustomCalendarAdapter(customCalendarViewModel, lifecycleOwner)
+        binding.rvCalendarDates.apply {
+            setHasFixedSize(true)
+            adapter = customCalendarAdapter
+            itemAnimator = null
+        }
+        updateCalendar()
+    }
+
+    private fun setLifecycleOwner() {
+        lifecycleOwner = if (context is LifecycleOwner) {
+            context as LifecycleOwner
+        } else {
+            (context as ContextWrapper).baseContext as LifecycleOwner
+        }
+    }
+
+    private fun updateCalendar() {
+        binding.currentMonth = baseMonthFormatter.format(calInstance.time)
+        dates = ArrayList()
+        month = calInstance.get(Calendar.MONTH)
+        year = calInstance.get(Calendar.YEAR)
+
+        calInstance.set(Calendar.DAY_OF_MONTH, 1)
+        calInstance.apply {
+            set(Calendar.HOUR, 0)
+            set(Calendar.MINUTE, 0)
+            set(Calendar.SECOND, 0)
+            set(Calendar.MILLISECOND, 0)
+        }
+
+        val monthBeginningCell = calInstance.get(Calendar.DAY_OF_WEEK) - 1
+        calInstance.add(Calendar.DAY_OF_MONTH, -monthBeginningCell)
+
+        while (dates.size < 42) {
+            dates.add(calInstance.time)
+            calInstance.add(Calendar.DAY_OF_MONTH, 1)
+        }
+        customCalendarAdapter.submitList(dates)
+    }
+
+    private fun initControl() {
+        binding.btnCalendarPastMonth.setOnClickListener {
+            goToPastMonth()
+        }
+        binding.btnCalendarNextMonth.setOnClickListener {
+            goToNextMonth()
+        }
+    }
+
+    private fun goToPastMonth() {
+        calInstance.set(Calendar.YEAR, year)
+        calInstance.set(Calendar.MONTH, month)
+        calInstance.add(Calendar.MONTH, -1)
+        updateCalendar()
+    }
+
+    private fun goToNextMonth() {
+        calInstance.set(Calendar.YEAR, year)
+        calInstance.set(Calendar.MONTH, month)
+        calInstance.add(Calendar.MONTH, 1)
+        updateCalendar()
+    }
+
+}
---
+++ common-ui/src/main/java/org/swm/att/common_ui/util/calendar/CustomCalendarUtil.kt
@@ -0,0 +1,96 @@
+package org.swm.att.common_ui.util.calendar
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.databinding.ViewDataBinding
+import androidx.lifecycle.LifecycleOwner
+import androidx.recyclerview.widget.RecyclerView.ViewHolder
+import org.swm.att.common_ui.R
+import org.swm.att.common_ui.base.BaseListAdapter
+import org.swm.att.common_ui.databinding.CustomCalendarDateBinding
+import org.swm.att.common_ui.util.ItemDiffCallback
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
+
+class CustomCalendarAdapter(
+    private val customCalendarViewModel: CustomCalendarViewModel,
+    private val lifecycleOwner: LifecycleOwner
+): BaseListAdapter<Date, CustomCalendarDateViewHolder> (
+    ItemDiffCallback(
+        onItemsTheSame = { old, new -> old == new },
+        onContentTheSame = { old, new -> old == new }
+    )
+) {
+    private val baseDateFormat = SimpleDateFormat("dd", Locale.KOREA)
+
+    override fun inflateBinding(inflater: LayoutInflater, parent: ViewGroup): ViewDataBinding {
+        return CustomCalendarDateBinding.inflate(inflater, parent, false)
+    }
+
+    override fun createViewHolder(binding: ViewDataBinding): CustomCalendarDateViewHolder {
+        return CustomCalendarDateViewHolder(binding as CustomCalendarDateBinding, customCalendarViewModel, lifecycleOwner)
+    }
+
+    override fun bindViewHolder(holder: CustomCalendarDateViewHolder, item: Date) {
+        holder.bind(item, baseDateFormat.format(item))
+        holder.itemView.apply {
+            setOnClickListener {
+                if (customCalendarViewModel.startDate.value == null) {
+                    customCalendarViewModel.setStartDate(item)
+                } else {
+                    if (customCalendarViewModel.endDate.value == null) {
+                        if (customCalendarViewModel.startDate.value!! >= item) {
+                            customCalendarViewModel.setStartDate(item)
+                        } else {
+                            customCalendarViewModel.setEndDate(item)
+                        }
+                    } else {
+                        customCalendarViewModel.setStartDate(item)
+                    }
+                }
+            }
+        }
+    }
+}
+
+class CustomCalendarDateViewHolder(
+    private val binding: CustomCalendarDateBinding,
+    private val viewModel: CustomCalendarViewModel,
+    private val lifecycleOwner: LifecycleOwner
+): ViewHolder(binding.root) {
+    fun bind(date: Date, strDate: String) {
+        binding.date = strDate
+        setSelectingDateObserver(date)
+    }
+
+    private fun setSelectingDateObserver(date: Date) {
+        viewModel.startDate.observe(lifecycleOwner) {
+            changeBackground(date)
+        }
+        viewModel.endDate.observe(lifecycleOwner) {
+            changeBackground(date)
+        }
+    }
+
+    private fun changeBackground(date: Date) {
+        val background = if(viewModel.endDate.value == null) {
+            if (viewModel.startDate.value == date) {
+                R.drawable.shape_oval_date
+            } else {
+                R.color.white
+            }
+        } else {
+            if (viewModel.startDate.value == date) {
+                R.drawable.shape_round_start_date
+            } else if (viewModel.endDate.value == date) {
+                R.drawable.shape_round_end_date
+            } else if (date in viewModel.startDate.value!!..viewModel.endDate.value!!) {
+                R.color.main_trans
+            } else {
+                R.color.white
+            }
+        }
+        binding.tvCalendarDate.setBackgroundResource(background)
+    }
+}
