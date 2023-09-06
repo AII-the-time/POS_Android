f8032b2d8dcf6334d2f1a0823ea11d57f2542b97
915dbfl <dbfl00915@naver.com>
2023-09-06 13:35:00 +0900

    feat: add preorder filtering dialog #51

---
+++ feature/main/src/main/java/org/swm/att/home/preorder/DialogPreorderFiltering.kt
@@ -0,0 +1,12 @@
+package org.swm.att.home.preorder
+
+import org.swm.att.common_ui.base.BaseDialogFiltering
+import java.util.Date
+
+class DialogPreorderFiltering(
+    private val preorderViewModel: PreorderViewModel
+): BaseDialogFiltering() {
+    override fun onFilteringBtnClick(startDate: Date, endDate: Date?) {
+        preorderViewModel.getPreordersForFilteringDates(startDate, endDate)
+    }
+}
