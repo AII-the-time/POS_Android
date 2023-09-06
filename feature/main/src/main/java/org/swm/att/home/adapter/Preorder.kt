37dfd271e3cce980f39b75cd6791c0c5ee890cc8
915dbfl <dbfl00915@naver.com>
2023-09-06 13:44:57 +0900

    feat: add and apply preorderList adapter/viewholder #51

---
+++ feature/main/src/main/java/org/swm/att/home/adapter/Preorder.kt
@@ -0,0 +1,52 @@
+package org.swm.att.home.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.databinding.ViewDataBinding
+import androidx.recyclerview.widget.RecyclerView
+import org.swm.att.common_ui.base.BaseListAdapter
+import org.swm.att.common_ui.util.ItemDiffCallback
+import org.swm.att.domain.entity.response.PreorderVO
+import org.swm.att.home.databinding.ItemPreorderBinding
+import org.swm.att.home.preorder.PreorderViewModel
+
+class PreorderListItemAdapter(
+    private val preorderViewModel: PreorderViewModel,
+    private val isValid: Boolean
+): BaseListAdapter<PreorderVO, PreorderItemViewHolder>(
+    ItemDiffCallback(
+        onItemsTheSame = { old, new -> old.orderId == new.orderId },
+        onContentTheSame = { old, new -> old == new }
+    )
+) {
+    override fun inflateBinding(inflater: LayoutInflater, parent: ViewGroup): ViewDataBinding {
+        return ItemPreorderBinding.inflate(inflater, parent, false)
+    }
+
+    override fun createViewHolder(binding: ViewDataBinding): PreorderItemViewHolder {
+        return PreorderItemViewHolder(binding as ItemPreorderBinding)
+    }
+
+    override fun onBindViewHolder(holder: PreorderItemViewHolder, position: Int) {
+        super.onBindViewHolder(holder, position)
+        val item = getItem(position)
+        holder.itemView.setOnClickListener {
+            // 추후 api 연동 및 실제 데이터 사용
+            preorderViewModel.getSelectedPreorderDetail(item.orderId)
+            preorderViewModel.setCurrentSelectedPreorderId(position, isValid)
+        }
+    }
+
+    override fun bindViewHolder(holder: PreorderItemViewHolder, item: PreorderVO) {
+        holder.bind(item)
+    }
+
+}
+
+class PreorderItemViewHolder(
+    private val binding: ItemPreorderBinding
+): RecyclerView.ViewHolder(binding.root) {
+    fun bind(preorder: PreorderVO) {
+        binding.preorder = preorder
+    }
+}
