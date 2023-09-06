b67835e07416edc52d21fb66b6b0be2ceba27fd9
915dbfl <dbfl00915@naver.com>
2023-09-06 13:31:36 +0900

    feat: add base filtering dialog #51

---
+++ common-ui/src/main/java/org/swm/att/common_ui/base/BaseDialogFiltering.kt
@@ -0,0 +1,44 @@
+package org.swm.att.common_ui.base
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.viewModels
+import org.swm.att.common_ui.R
+import org.swm.att.common_ui.databinding.BaseDialogFilteringBinding
+import org.swm.att.common_ui.util.calendar.CustomCalendarViewModel
+import java.util.Date
+
+abstract class BaseDialogFiltering : BaseDialog<BaseDialogFilteringBinding>(R.layout.base_dialog_filtering) {
+    private val customCalendarViewModel by viewModels<CustomCalendarViewModel>()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setDialogClosedBtnClickListener()
+        initCustomCalendar()
+        setBtnFilteringSearchClickListener()
+    }
+
+    private fun setDialogClosedBtnClickListener() {
+        binding.btnCloseFilteringDialog.setOnClickListener {
+            dismiss()
+        }
+    }
+
+    private fun initCustomCalendar() {
+        binding.customCalendar.initCustomCalendarViewModel(customCalendarViewModel)
+    }
+
+    abstract fun onFilteringBtnClick(startDate: Date, endDate: Date?)
+
+
+    private fun setBtnFilteringSearchClickListener() {
+        binding.btnFilteringSearch.setOnClickListener {
+            dismiss()
+            val startDate = customCalendarViewModel.startDate.value
+            val endDate = customCalendarViewModel.endDate.value
+            if (startDate != null) {
+                onFilteringBtnClick(startDate, endDate)
+            }
+        }
+    }
+}
