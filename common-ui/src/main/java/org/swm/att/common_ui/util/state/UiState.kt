4638659fa8b2dc6cc577968dee42e7f41d96d870
915dbfl <dbfl00915@naver.com>
2023-09-11 14:08:39 +0900

    feat: add UiState #54

---
+++ common-ui/src/main/java/org/swm/att/common_ui/util/state/UiState.kt
@@ -0,0 +1,8 @@
+package org.swm.att.common_ui.util.state
+
+sealed class UiState<out T> {
+    // Loading의 경우, 상태의 데이터가 필요 없기 때문에 단 하나의 인스턴스만 있어도 된다.
+    object Loading: UiState<Nothing>()
+    data class Success<T>(val data: T): UiState<T>()
+    data class Error<T>(val errorMsg: String?): UiState<T>()
+}
