563bad05cad1bd0aa700e8462c01b6ced87f0b0c
915dbfl <dbfl00915@naver.com>
2023-09-24 18:48:33 +0900

    feat: display recipe / option info of mock data #53

--- feature/main/src/main/java/org/swm/att/home/recipe/RecipeFragment.kt
+++ feature/main/src/main/java/org/swm/att/home/recipe/RecipeFragment.kt
@@ -1,9 +1,157 @@
 package org.swm.att.home.recipe
 
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Toast
+import androidx.core.view.get
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.android.material.chip.Chip
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
 import org.swm.att.common_ui.base.BaseFragment
+import org.swm.att.common_ui.util.state.UiState
+import org.swm.att.domain.entity.response.CategoriesVO
 import org.swm.att.home.R
+import org.swm.att.home.adapter.BaseInteractiveItemAdapter
+import org.swm.att.home.adapter.SelectableItemAdapter
 import org.swm.att.home.databinding.FragmentRecipeBinding
 
+@AndroidEntryPoint
 class RecipeFragment : BaseFragment<FragmentRecipeBinding>(R.layout.fragment_recipe) {
-    //todo
+    private val recipeViewModel by viewModels<RecipeViewModel>()
+    private lateinit var registeredMenusAdapter: SelectableItemAdapter
+    private lateinit var recipesAdapter: BaseInteractiveItemAdapter
+    private lateinit var optionsAdapter: BaseInteractiveItemAdapter
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initMenusRecyclerView()
+        setObserver()
+        setDataBinding()
+        initData()
+    }
+
+    private fun initMenusRecyclerView() {
+        registeredMenusAdapter = SelectableItemAdapter(recipeViewModel)
+        binding.rvCategoryMenuList.apply {
+            setHasFixedSize(true)
+            layoutManager = LinearLayoutManager(requireContext())
+            adapter = registeredMenusAdapter
+        }
+
+        recipesAdapter = BaseInteractiveItemAdapter(recipeViewModel)
+        binding.rvMenuRecipe.apply {
+            setHasFixedSize(false)
+            layoutManager = LinearLayoutManager(requireContext())
+            adapter = recipesAdapter
+        }
+
+        optionsAdapter = BaseInteractiveItemAdapter(recipeViewModel)
+        binding.rvMenuOptions.apply {
+            setHasFixedSize(false)
+            layoutManager = LinearLayoutManager(requireContext())
+            adapter = optionsAdapter
+        }
+    }
+
+    private fun setObserver() {
+        lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.STARTED) {
+                recipeViewModel.getCategories.collect { uiState ->
+                    when (uiState) {
+                        is UiState.Success -> {
+                            uiState.data?.let {
+                                setCategoryChips(it)
+                            }
+                        }
+                        is UiState.Error -> Toast.makeText(
+                            requireContext(),
+                            uiState.errorMsg,
+                            Toast.LENGTH_SHORT
+                        ).show()
+
+                        is UiState.Loading -> { /* nothing */ }
+                    }
+                }
+            }
+        }
+        recipeViewModel.currentSelectedMenuId.observe(viewLifecycleOwner) {
+            val pastId = recipeViewModel.selectedMenuId.value
+            binding.rvCategoryMenuList[it].setBackgroundResource(R.color.main_trans)
+            recipeViewModel.changeSelectedState()
+            pastId?.let { pastId ->
+                if (pastId != it) {
+                    recipeViewModel.selectedMenuId.value?.let { pastId ->
+                        binding.rvCategoryMenuList[pastId].setBackgroundResource(R.color.white)
+                    }
+                }
+            }
+        }
+        recipeViewModel.selectedCategory.observe(viewLifecycleOwner) {
+            registeredMenusAdapter.submitList(it.menus)
+            binding.edtCategoryName.setText(String.format("%s(%d건)", it.category, it.menus.size))
+        }
+        recipeViewModel.selectedMenuInfo.observe(viewLifecycleOwner) {
+            Log.d("RecipeFragment", "setObserver: ${it.recipe}")
+            Log.d("RecipeFragment", "setObserver: ${it.option}")
+            recipesAdapter.submitList(it.recipe)
+            optionsAdapter.submitList(it.option)
+        }
+    }
+
+    private fun setCategoryChips(categoriesVO: CategoriesVO) {
+        categoriesVO.categories.let {
+            // category chip 추가
+            for (index in it.indices) {
+                val type = it[index]
+                val chip = layoutInflater.inflate(R.layout.item_category_chip, binding.cgMenuCategories, false) as Chip
+                chip.apply {
+                    tag = type
+                    id = index
+                    text = type.category
+                    isChecked = if (index == 0) {
+                        recipeViewModel.setSelectedCategory(type)
+                        true
+                    } else {
+                        false
+                    }
+                    setOnCheckedChangeListener { _, isChecked ->
+                        if (isChecked) {
+                            recipeViewModel.setSelectedCategory(type)
+                        }
+                    }
+                }
+                binding.cgMenuCategories.addView(chip)
+            }
+            addAddItemChip(it.size)
+        }
+    }
+
+    private fun addAddItemChip(chipId: Int) {
+        // addItemChip의 경우, 항상 추가
+        val addItemChip = layoutInflater.inflate(
+            R.layout.item_category_chip,
+            binding.cgMenuCategories,
+            false
+        ) as Chip
+        addItemChip.apply {
+            id = chipId
+            text = "+"
+        }
+        binding.cgMenuCategories.addView(addItemChip)
+    }
+
+    private fun setDataBinding() {
+        binding.recipeViewModel = recipeViewModel
+    }
+
+    private fun initData() {
+        //임시로 1번 storeId로 설정
+        recipeViewModel.getRegisteredMenus(1)
+    }
+
 }
