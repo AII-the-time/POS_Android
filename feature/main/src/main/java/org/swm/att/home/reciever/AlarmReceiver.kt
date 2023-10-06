3212ec2e1d3a5bdcca2060c1487b2f3eb1e996b3
915dbfl <dbfl00915@naver.com>
2023-10-06 21:35:49 +0900

    feat: add AlarmReceiver #77

---
+++ feature/main/src/main/java/org/swm/att/home/reciever/AlarmReceiver.kt
@@ -0,0 +1,14 @@
+package org.swm.att.home.reciever
+
+import android.app.PendingIntent
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import org.swm.att.common_ui.util.alarm.PreorderAlarmDialog
+
+class AlarmReceiver : BroadcastReceiver() {
+    override fun onReceive(p0: Context?, p1: Intent?) {
+        val intent = Intent(p0, PreorderAlarmDialog::class.java)
+        PendingIntent.getActivity(p0, 0, intent, PendingIntent.FLAG_IMMUTABLE).send()
+    }
+}
