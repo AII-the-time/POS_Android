29c09b36664818cdd0ead0cd1cf8afc7fb87e556
915dbfl <dbfl00915@naver.com>
2023-10-06 21:34:14 +0900

    feat: move function of processing systemUi to BaseActivity #77

--- feature/main/src/main/java/org/swm/att/home/MainActivity.kt
+++ feature/main/src/main/java/org/swm/att/home/MainActivity.kt
@@ -1,12 +1,5 @@
 package org.swm.att.home
 
-import android.content.Context
-import android.os.Build
-import android.view.MotionEvent
-import android.view.View
-import android.view.WindowInsets
-import android.view.WindowInsetsController
-import android.view.inputmethod.InputMethodManager
 import android.widget.CheckBox
 import androidx.activity.viewModels
 import androidx.navigation.NavController
@@ -28,8 +21,6 @@ class MainActivity: BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
         checkRefreshToken()
         setNavController()
         setBindingData()
-        removeStatusBar()
-        hideSystemUI()
         setObserver()
     }
 
@@ -38,7 +29,8 @@ class MainActivity: BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
     }
 
     private fun setNavController() {
-        val navHost = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navHost =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
         navController = navHost.navController
     }
 
@@ -47,37 +39,6 @@ class MainActivity: BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
         val dateFormat = SimpleDateFormat("MM월 dd일 HH시 mm분", Locale.KOREA)
         binding.barDate = dateFormat.format(date)
         binding.mainViewModel = mainViewModel
-    }
-
-    @Suppress("DEPRECATION")
-    private fun removeStatusBar() {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
-            window.setDecorFitsSystemWindows(false)
-            window.insetsController?.hide(WindowInsets.Type.statusBars())
-        } else {
-            window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
-        }
-    }
-
-    @Suppress("DEPRECATION")
-    private fun hideSystemUI() {
-        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
-            val controller = window.insetsController
-            if(controller != null) {
-                controller.hide(WindowInsets.Type.navigationBars())
-                controller.systemBarsBehavior = WindowInsetsController.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
-            }
-        } else {
-            window.decorView.systemUiVisibility = (
-                    View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                            or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION)
-        }
-    }
-
-    override fun dispatchTouchEvent(ev: MotionEvent?): Boolean {
-        val imm  = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
-        imm.hideSoftInputFromWindow(currentFocus?.windowToken, 0)
-        return super.dispatchTouchEvent(ev)
     }
 
     private fun setObserver() {
