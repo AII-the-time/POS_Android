683f5c15fa8381bbd1f7c2c5f35af8291890c13b
915dbfl <dbfl00915@naver.com>
2023-10-07 01:29:42 +0900

    feat: add Date extension for UTC/RTC

---
+++ common-ui/src/main/java/org/swm/att/common_ui/util/Extension.kt
@@ -0,0 +1,25 @@
+package org.swm.att.common_ui.util
+
+import java.util.Calendar
+import java.util.Date
+
+fun Date.getUTCDateTime(): Date {
+    val localTime = Calendar.getInstance()
+    localTime.time = this
+    localTime.add(Calendar.HOUR, -9)
+    return localTime.time
+}
+
+fun Date.getRTCDateTime(): Date {
+    val localTime = Calendar.getInstance()
+    localTime.time = this
+    localTime.add(Calendar.HOUR, 9)
+    return localTime.time
+}
+
+fun Date.getRTCDateTimeFromData(data: Date): Date {
+    val localTime = Calendar.getInstance()
+    localTime.time = data
+    localTime.add(Calendar.HOUR, 9)
+    return localTime.time
+}
