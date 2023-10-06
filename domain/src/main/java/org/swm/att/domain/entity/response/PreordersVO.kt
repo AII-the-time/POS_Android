4ca650a7d1f0d1faf4bd1113b8f7699f635cd01b
915dbfl <dbfl00915@naver.com>
2023-10-06 19:55:16 +0900

    feat: update preorder dto/vo component
    - 수정된 api 명세서에 따라 dto/vo 업데이트

--- domain/src/main/java/org/swm/att/domain/entity/response/PreordersVO.kt
+++ domain/src/main/java/org/swm/att/domain/entity/response/PreordersVO.kt
@@ -1,6 +1,7 @@
 package org.swm.att.domain.entity.response
 
 data class PreOrdersVO(
-    val endPage: Int,
+    val lastPage: Int,
+    val totalPreorderCount: Int,
     val preOrders: List<PreorderVO>
 )
