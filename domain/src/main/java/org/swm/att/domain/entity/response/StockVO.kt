f9833c1f09e69a438e6d9a3cc26a06a68fe6ecb1
915dbfl <dbfl00915@naver.com>
2023-10-20 13:55:10 +0900

    feat: add isNewItem value in StockVO #100

--- domain/src/main/java/org/swm/att/domain/entity/response/StockVO.kt
+++ domain/src/main/java/org/swm/att/domain/entity/response/StockVO.kt
@@ -3,5 +3,6 @@ package org.swm.att.domain.entity.response
 data class StockVO(
     val id: Int,
     val name: String,
-    val isMixed: Boolean
+    val isMixed: Boolean,
+    val isNew: Boolean? = false
 )
