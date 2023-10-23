bb42f33f9d5ea80ae0599229b597f2e5958f66c1
915dbfl <dbfl00915@naver.com>
2023-10-24 01:02:54 +0900

    feat: set nullable values of StockVO #105

--- domain/src/main/java/org/swm/att/domain/entity/response/StockVO.kt
+++ domain/src/main/java/org/swm/att/domain/entity/response/StockVO.kt
@@ -3,11 +3,11 @@ package org.swm.att.domain.entity.response
 data class StockVO(
     val id: Int? = -1,
     val name: String,
-    val amount: Int,
-    val unit: String,
-    val price: String,
-    val currentAmount: Int,
-    val noticeThreshold: Int,
-    val updatedAt: String,
+    val amount: Int?,
+    val unit: String?,
+    val price: String?,
+    val currentAmount: Int?,
+    val noticeThreshold: Int?,
+    val updatedAt: String?,
     val menus: List<String>? = null
 )
