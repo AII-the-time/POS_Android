bf2c5ccc49d2f8c98ba155af90f670f1cf919e79
915dbfl <dbfl00915@naver.com>
2023-10-25 12:46:18 +0900

    feat: add billCancelConfirmDialog #96

---
+++ feature/main/src/main/java/org/swm/att/home/bills/BillCancelConfirmDialog.kt
@@ -0,0 +1,12 @@
+package org.swm.att.home.bills
+
+import org.swm.att.common_ui.presenter.base.BaseConfirmDialog
+import org.swm.att.common_ui.R
+
+class BillCancelConfirmDialog(
+    private val billViewModel: BillViewModel
+): BaseConfirmDialog(R.string.tv_confirm_bill_cancel_text) {
+    override fun deleteItem() {
+        billViewModel.deleteOrder()
+    }
+}
