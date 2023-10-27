e546a4a235467ae4cd7fb96340164a1487b9928f
915dbfl <dbfl00915@naver.com>
2023-10-27 13:24:11 +0900

    feat: add categoryDeleteConfirmDialog #96

---
+++ feature/main/src/main/java/org/swm/att/home/recipe/CategoryDeleteConfirmDialog.kt
@@ -0,0 +1,12 @@
+package org.swm.att.home.recipe
+
+import org.swm.att.common_ui.presenter.base.BaseConfirmDialog
+import org.swm.att.common_ui.R
+
+class CategoryDeleteConfirmDialog (
+    private val recipeViewModel: RecipeViewModel
+): BaseConfirmDialog(R.string.tv_confirm_category_delete_text) {
+    override fun deleteItem() {
+        recipeViewModel.deleteCategory()
+    }
+}
