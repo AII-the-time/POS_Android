ab5400343b7f451d6b9f2c9893d917d5d8aad3b2
915dbfl <dbfl00915@naver.com>
2023-10-31 20:41:25 +0900

    feat: apply certification process #122

--- feature/main/src/main/java/org/swm/att/home/login/LoginActivity.kt
+++ feature/main/src/main/java/org/swm/att/home/login/LoginActivity.kt
@@ -1,8 +1,82 @@
 package org.swm.att.home.login
 
+import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.viewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
 import org.swm.att.common_ui.presenter.base.BaseActivity
+import org.swm.att.common_ui.state.UiState
 import org.swm.att.home.R
 import org.swm.att.home.databinding.ActivityLoginBinding
 
+@AndroidEntryPoint
 class LoginActivity: BaseActivity<ActivityLoginBinding>(R.layout.activity_login) {
+    private val loginViewModel by viewModels<LoginViewModel>()
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setPostPhoneNumberStateObserver()
+        setCheckCertificationCodeStateObserver()
+        setSubmitPhoneNumberBtnClickListener()
+        setSubmitAuthenticationBtnClickListener()
+    }
+
+    //observe
+    private fun setPostPhoneNumberStateObserver() {
+        lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.STARTED) {
+                loginViewModel.postPhoneNumberState.collect { uiState ->
+                    when(uiState) {
+                        is UiState.Success -> {
+                            Toast.makeText(this@LoginActivity, "인증번호가 전송되었습니다!", Toast.LENGTH_SHORT).show()
+                        }
+                        is UiState.Loading -> {/* nothing */}
+                        is UiState.Error -> Toast.makeText(this@LoginActivity, uiState.errorMsg, Toast.LENGTH_SHORT).show()
+                    }
+                }
+            }
+        }
+    }
+
+    private fun setCheckCertificationCodeStateObserver() {
+        lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.STARTED) {
+                loginViewModel.checkCertificationCodeState.collect { uiState ->
+                    when(uiState) {
+                        is UiState.Success -> {
+                            Toast.makeText(this@LoginActivity, "인증되었습니다!", Toast.LENGTH_SHORT).show()
+                        }
+                        is UiState.Loading -> {/* nothing */}
+                        is UiState.Error -> Toast.makeText(this@LoginActivity, uiState.errorMsg, Toast.LENGTH_SHORT).show()
+                    }
+                }
+            }
+        }
+    }
+
+    //btn click listener
+    private fun setSubmitPhoneNumberBtnClickListener() {
+        binding.btnSubmitPhoneNumber.setOnClickListener {
+            val phoneNumber = binding.tiePhoneNumber.text.toString()
+            if (!phoneNumber.isNullOrEmpty()) {
+                loginViewModel.postPhoneNumberForAuthentication(phoneNumber)
+            } else {
+                Toast.makeText(this@LoginActivity, "전화번호를 확인해주세요!", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    private fun setSubmitAuthenticationBtnClickListener() {
+        binding.btnSubmitAuthenticationCode.setOnClickListener {
+            val code = binding.tieAuthenticationCode.text
+            if (!code.isNullOrEmpty()) {
+                loginViewModel.checkCertificationCode(code.toString())
+            } else {
+                Toast.makeText(this@LoginActivity, "올바른 인증번호를 입력해주세요!", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
 }
---
+++ feature/main/src/main/java/org/swm/att/home/login/LoginViewModel.kt
@@ -0,0 +1,82 @@
+package org.swm.att.home.login
+
+import androidx.lifecycle.viewModelScope
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
+import org.swm.att.common_ui.presenter.base.BaseViewModel
+import org.swm.att.common_ui.state.UiState
+import org.swm.att.domain.entity.HttpResponseException
+import org.swm.att.domain.entity.request.CertificationVO
+import org.swm.att.domain.entity.response.CertificatedPhoneTokenVO
+import org.swm.att.domain.entity.response.TokenForCertificationPhoneVO
+import org.swm.att.domain.repository.AttPosUserRepository
+import java.lang.IllegalArgumentException
+import javax.inject.Inject
+
+@HiltViewModel
+class LoginViewModel @Inject constructor(
+    private val attPosUserRepository: AttPosUserRepository
+): BaseViewModel() {
+    //phoneNumber
+    private val _postPhoneNumberState = MutableStateFlow<UiState<TokenForCertificationPhoneVO>>(UiState.Loading)
+    val postPhoneNumberState: StateFlow<UiState<TokenForCertificationPhoneVO>> = _postPhoneNumberState
+
+    //certificationCode
+    private val _checkCertificationCodeState = MutableStateFlow<UiState<CertificatedPhoneTokenVO>>(UiState.Loading)
+    val checkCertificationCodeState: StateFlow<UiState<CertificatedPhoneTokenVO>> = _checkCertificationCodeState
+
+    //local
+    private var tokenForCertificatePhone: String? = null
+    private var phone: String? = null
+
+    //phoneNumber
+    fun postPhoneNumberForAuthentication(phoneNumber: String) {
+        phone = phoneNumber
+        viewModelScope.launch(attExceptionHandler) {
+            attPosUserRepository.postPhoneNumberForAuthentication(phoneNumber)
+                .collect { result ->
+                    result.onSuccess {
+                        tokenForCertificatePhone = it.tokenForCertificatePhone
+                        _postPhoneNumberState.value = UiState.Success(it)
+                    }.onFailure {
+                        val errorMsg = if (it is HttpResponseException) it.message else "전화번호 전송 실패"
+                        _postPhoneNumberState.value = UiState.Error(errorMsg)
+                    }
+                }
+        }
+    }
+
+    //certificationCode
+    fun checkCertificationCode(code: String) {
+        try {
+            viewModelScope.launch(attExceptionHandler) {
+                val certificationBody = checkCreateCertificationBody(code)
+                attPosUserRepository.checkCertificationCode(certificationBody).collect { result ->
+                    result.onSuccess {
+                        _checkCertificationCodeState.value = UiState.Success(it)
+                    }.onFailure {
+                        val errorMsg = if (it is HttpResponseException) it.message else "인증번호 확인에 실패하였습니다.\n 다시 시도해주세요!"
+                        _checkCertificationCodeState.value = UiState.Error(errorMsg)
+                    }
+                }
+            }
+        } catch (e: IllegalArgumentException) {
+            _checkCertificationCodeState.value = UiState.Error(e.message)
+        }
+    }
+
+    //local
+    private fun checkCreateCertificationBody(code: String): CertificationVO {
+        try {
+            return CertificationVO(
+                phoneCertificationToken = requireNotNull(tokenForCertificatePhone),
+                phone = requireNotNull(phone),
+                certificationCode = code
+            )
+        } catch (e: Exception) {
+            throw IllegalArgumentException("전화번호 입력 후, 전송된 인증번호를 입력해주세요!")
+        }
+    }
+}
