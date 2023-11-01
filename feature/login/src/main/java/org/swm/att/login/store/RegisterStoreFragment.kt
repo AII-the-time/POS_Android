bc7c19be8036af5d1ca279393f1a4fc0586497be
915dbfl <dbfl00915@naver.com>
2023-11-01 15:06:14 +0900

    feat: add register store fragment #122

---
+++ feature/login/src/main/java/org/swm/att/login/store/RegisterStoreFragment.kt
@@ -0,0 +1,8 @@
+package org.swm.att.login.store
+
+import org.swm.att.common_ui.presenter.base.BaseFragment
+import org.swm.att.login.R
+import org.swm.att.login.databinding.FragmentRegisterStoreBinding
+
+class RegisterStoreFragment : BaseFragment<FragmentRegisterStoreBinding>(R.layout.fragment_register_store) {
+}
