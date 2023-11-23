071698b2b0ec6da824533756bb47ed07fca462c6
915dbfl <dbfl00915@naver.com>
2023-11-23 17:00:34 +0900

    feat: fix name of chartColor #166

--- domain/src/main/java/org/swm/att/domain/sever_driven_ui/response/piechart/ReportPieChartItemVO.kt
+++ domain/src/main/java/org/swm/att/domain/sever_driven_ui/response/piechart/ReportPieChartItemVO.kt
@@ -2,6 +2,6 @@ package org.swm.att.domain.sever_driven_ui.response.piechart
 
 data class ReportPieChartItemVO(
     val categoryName: String,
-    val chartColor: String,
+    val charColor: String,
     val categoryCount: Int
 )
