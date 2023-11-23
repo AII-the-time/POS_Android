cb828651b4f23c0ff2c547c27a14f46f458fe76f
915dbfl <dbfl00915@naver.com>
2023-11-23 16:05:57 +0900

    feat: add amount component in historyDTO/VO #169

--- data/src/main/java/org/swm/att/data/remote/response/HistoryDTO.kt
+++ data/src/main/java/org/swm/att/data/remote/response/HistoryDTO.kt
@@ -6,12 +6,15 @@ import org.swm.att.domain.entity.response.HistoryVO
 
 @JsonClass(generateAdapter = true)
 data class HistoryDTO(
+    @field:Json(name = "amount")
+    val amount: Int?,
     @field:Json(name = "date")
     val date: String?,
     @field:Json(name = "price")
     val price: String?
 ) {
     fun toVO() = HistoryVO(
+        amount = amount ?: 0,
         date = date ?: "Unknown",
         price = price ?: "0"
     )
--- domain/src/main/java/org/swm/att/domain/entity/response/HistoryVO.kt
+++ domain/src/main/java/org/swm/att/domain/entity/response/HistoryVO.kt
@@ -1,6 +1,7 @@
 package org.swm.att.domain.entity.response
 
 data class HistoryVO(
+    val amount: Int,
     val date: String,
     val price: String
 )
